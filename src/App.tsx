01f61d7d2655369254266c8b15928b7ccf9c2a9f
Thomas Jonstrup <thomas_jonstrup@hotmail.com>
2020-05-17 16:50:40 +0200

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./tailwind.generated.css";
+
+import data from './data.json'
+
+import HeaderBg from './images/bg-header-desktop.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<header className="bg-teal-500 mb-12">
+				<img src={HeaderBg} alt="bg-header" className="w-full" />
+			</header>
+			<section className="container m-auto">
+				{data.map((item) => {
+					console.log("item :>> ", item);
+					return (
+						<div
+							className="flex flex-col items-center bg-white shadow-lg my-16 p-6 lg:flex-row mx-6"
+							key={item.id}
+						>
+							<img
+								src={item.logo}
+								alt={item.position}
+								className="w-20 h-20 mb-4 lg:m-0 lg:mr-6"
+							/>
+							<h2>{item.position}</h2>
+						</div>
+					);
+				})}
+			</section>
+			<footer className="container flex flex-row items-center justify-center">
+				<div className="attribution">
+					Challenge by{" "}
+					<a
+						href="https://www.frontendmentor.io?ref=challenge"
+						target="_blank"
+					>
+						Frontend Mentor
+					</a>
+					. Coded by{" "}
+					<a href="https://github.com/thomasjonstrup">
+						Thomas Jonstrup
+					</a>
+					.
+				</div>
+			</footer>
+		</>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+		theme: {
+		extend: {
+			colors: {
+				"regal-blue": "var(--color-primary)",
+				"light-grayish-cyan": "var(--light-grayish-cyan)",
+				"dark-grayish-cyan": "var(--dark-grayish-cyan)",
+				"very-dark-grayish-cyan": "var(--very-dark-grayish-cyan)"
+			},
+		},
+	}/* ,
+	theme: {
+		colors: {
+			"regal-blue": "var(--color-primary)",
+		},
+	}, */
+};
